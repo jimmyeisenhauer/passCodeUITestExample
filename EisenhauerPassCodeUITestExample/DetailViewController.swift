6c591800809ed5a1f94791a1957d36f60d052475
James Eisenhauer <james.eisenhauer@gmail.com>
2019-05-07 18:53:30 -0700

    Initial Commit

---
+++ EisenhauerPassCodeUITestExample/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  EisenhauerPassCodeUITestExample
+//
+//  Created by James Eisenhauer on 5/7/19.
+//  Copyright © 2019 James Eisenhauer. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
